ede3f19d9eaba1c8e89456cbb0957583d25b14b0
Fma965 <fma965@cyanlabs.net>
2021-05-03 23:42:51 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace DeleteAllExceptList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDry = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.path = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exclusions = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDry
+            // 
+            this.btnDry.Location = new System.Drawing.Point(253, 242);
+            this.btnDry.Name = "btnDry";
+            this.btnDry.Size = new System.Drawing.Size(73, 29);
+            this.btnDry.TabIndex = 0;
+            this.btnDry.Text = "Dry Run";
+            this.btnDry.UseVisualStyleBackColor = true;
+            this.btnDry.Click += new System.EventHandler(this.BtnDryClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(332, 242);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(70, 29);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "DELETE!";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDeleteClick);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(51, 18);
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Size = new System.Drawing.Size(283, 20);
+            this.path.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(338, 18);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(64, 20);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path: ";
+            // 
+            // exclusions
+            // 
+            this.exclusions.Location = new System.Drawing.Point(10, 74);
+            this.exclusions.Multiline = true;
+            this.exclusions.Name = "exclusions";
+            this.exclusions.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.exclusions.Size = new System.Drawing.Size(392, 164);
+            this.exclusions.TabIndex = 5;
+            this.exclusions.WordWrap = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Excluded files:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.exclusions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnDry);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Delete All Files Except Exclusions!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDry;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox exclusions;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DeleteAllExceptList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnBrowseClick(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) path.Text = folderBrowserDialog1.SelectedPath;
+            btnDelete.Enabled = false;
+        }
+
+        private void BtnDryClick(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new(path.Text);
+            int count = d.GetFiles().Select(fi => fi.FullName.Replace(path.Text + "\\", "")).Count(filename => !exclusions.Text.Contains(filename));
+            MessageBox.Show(count + @" files will be deleted!",@"Dry Run Results");
+            btnDelete.Enabled = true;
+        }
+
+        private void BtnDeleteClick(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new(path.Text);
+            foreach (FileInfo fi in d.GetFiles())
+                if (!exclusions.Text.Contains(fi.FullName)) File.Delete(fi.FullName);
+        }
+    }
+}
